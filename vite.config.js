3a44386395ced6a5920d96104a02b4361cd3d43b
Sourabh <sourabhhaldarh@gmail.com>
2025-12-14 17:10:29 +0530

    feat: added netlify.toml file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import netlify from "@netlify/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), netlify()],
+  plugins: [react()],
 });

629729cc8374006a0e39d35986576011431eae15
Sourabh <sourabhhaldarh@gmail.com>
2025-12-14 20:59:24 +0530

    fix: deploy test

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,13 +4,11 @@ const AuthContext = createContext();
 
 function AuthProvider({ children }) {
   const [isAuthenticated, setIsAuthenticated] = useState(() => {
-    return localStorage.getItem("isAuthPharmEase") || false;
+    return localStorage.getItem("isAuthPharmEase") === "true";
   });
 
-  console.log(isAuthenticated);
-
   function login() {
-    localStorage.setItem("isAuthPharmEase", true);
+    localStorage.setItem("isAuthPharmEase", "true");
     setIsAuthenticated(true);
   }
 
